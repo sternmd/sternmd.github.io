0f78f9e3186af4428acbae3248c3b0085125c359
Max Stern <stern.maxwell@gmail.com>
2016-03-08 18:31:27 -0600

    Add comments, ideas to JS

--- project1/app.js
+++ project1/app.js
@@ -1,115 +1,85 @@
 $( document ).ready(function() {
 
-    // $("#red").animate({
+// Moves column IDs across screen.
+function loopRed() {
+
+    $('#red').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopRed();
+
+
+function loopOrange() {
+
+    $('#orange').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopOrange();
+
+
+function loopYellow() {
+
+    $('#yellow').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopYellow();
+
+
+function loopGreen() {
+
+    $('#green').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopGreen();
+
+function loopBlue() {
+
+    $('#blue').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopBlue();
+
+function loopIndigo() {
+
+    $('#indigo').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopIndigo();
+
+function loopViolet() {
+
+    $('#violet').animate ({
+        right: '+=100%',
+    }, 15000, 'linear');
+}
+loopViolet();
+
+    // $("#violet").animate({
     //     'marginLeft': '-=100%'
     // }, 15000);
 
-    function loopRed() {
-
-        $('#red').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopRed();
-
-
-    function loopOrange() {
-
-        $('#orange').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopOrange();
-
-
-    function loopYellow() {
-
-        $('#yellow').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopYellow();
-
-
-    function loopGreen() {
-
-        $('#green').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopGreen();
-
-    function loopBlue() {
-
-        $('#blue').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopBlue();
-
-    function loopIndigo() {
-
-        $('#indigo').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopIndigo();
-
-    function loopViolet() {
-
-        $('#violet').animate ({
-            right: '+=100%',
-        }, 15000, 'linear');
-    }
-    loopViolet();
-
-
-
-
-    // $("#orange").animate({
-    //     'marginLeft': '-=100%'
-    // }, 15000);
-
-    $("#yellow").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-    $("#green").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-    $("#blue").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-    $("#indigo").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-    $("#purple").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-    $("#violet").animate({
-        'marginLeft': '-=100%'
-    }, 15000);
-
-
-// write a function that returns a random number between 1 and 50
-// this is the range at which we want the colors r,g,b to vary.
-
+// Returns a random number between 1 and 50.
 function random() {
   return Math.floor(Math.random() * 50);
 };
 
+// Returns number ranging from 205 to 255, for RGB values.
 function randomColor() {
   return 255 - random();
 };
 
+// Returns 50% chance of adding or subtracting 150.
  function plusOrMinus() {
    return (Math.random() < 0.5 ? -random() +150 : random() +150)
 };
 
+// Returns "randomized" RGB strings.
 function randomRed() {
   return 'rgb(' + randomColor() + ',' + 0 + ',' + 0 + ')'
 }
@@ -142,7 +112,9 @@ function randomViolet() {
 // $("#red div :nth-child(2)").css("background-color",randomRed());
 // $("#red div :last").css("background-color",randomRed());
 
-//REDS
+//Injects randomized background colors to divs.
+
+// REDS
 $(".red1").each(function() {
     $(this).css("background-color", randomRed());
 });
@@ -233,26 +205,16 @@ $(".violet3").each(function() {
     $(this).css("background-color", randomViolet());
 });
 
+// Extracts all RGB values that are applied to divs within columns.
 var redColors = $('#red').children('div');
 for (var inc = 0; inc < 3; inc++) {
   console.log($(redColors[inc]).prop('style').backgroundColor);
   }
 
-//Returns all RGB values from the divs.
-
+// Returns single RGB value that is applied to divs within columns.
 var redOne = redColors.prop('style').backgroundColor;
 
-//Returns single RGB value a div.
-
-// Set a function to determine Luminance from a RGB value.
-// Extract the three RGB components individually.
-// Then use a standard formula to convert the resulting RGB values into perceived brightness.
-
-// --> colorSlicer(black)=0;
-// --> colorSlicker(yellow)=226;
-
-// The higher the luminance number, the brighter the color.
-
+// Convert RGB values into LUMINANCE (perceived brightness units).
 var color = 'rgb(255,150,0)'
 
 function colorSlicer(color) {
@@ -260,13 +222,38 @@ function colorSlicer(color) {
   var luminace = 0.299 * parseInt(colorArray[0]) + 0.587 * parseInt(colorArray[1]) + 0.114 * parseInt(colorArray[2]);
   return luminace;
 }
+// --> colorSlicer(black)   = 0;
+// --> colorSlicker(yellow) = 226;
+// The higher the luminance number, the brighter the color.
+
+// var highest = Math.max() ... ?
 
 // Find highest luminance of three divs. Set that to winnerDiv.
+// .append / AddClass winner??
+// for loop
+
 
 // var winnerDiv =
 
 // If user clicks winnerDiv, then column disappears .remove() and add +1 to scoreCounter.
 
+// $('div class/id winner').click(function(){
+// var scoreCounter =+ 1;
+// (this).remove();
+//});
+
 // Set condition if wrong div is click, game over.
+
 // Set condition if column hits left side of screen, game over.
+
+// var offScreen = $('div#').css({
+//       top: 0,
+//       left: 0});
+//
+// var gameOver = $('#gameover').text("<center><h1>G A M E O V E R</h1><center>");
+//
+// if OffScreen {
+//   return gameOver;
+// }
+
 });
